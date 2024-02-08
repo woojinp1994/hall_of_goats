9efa7ce322e39035314814d2f615d68f8d60baf0
Woojin Park <147019612+woojinp1994@users.noreply.github.com>
2024-02-08 15:35:37 -0800

    adding error exception

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -102,26 +102,37 @@ df_players = df_players.assign(player_race="")
 
 rowcount = df_players.shape[0]
 current_row = 0
+player_added = 0
+errors_on_add = []
 
 for row in df_players.itertuples():
-    url= f"https://tl.net/tlpd/korean/players/{row.player_id}_{row.player_name}"
+    try:
+        url= f"https://tl.net/tlpd/korean/players/{row.player_id}_{row.player_name}"
 
-    time.sleep(30)
-    browser = webdriver.Chrome()
-    browser.get(url)
-    time.sleep(5)
-    html = browser.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    race_info = soup.find('strong', string='Main Race').next_sibling.find_next(string=True)
-   
-    idx = row.Index
-    df_players.at[idx, 'player_race'] = race_info
-    current_row += 1
-    print(f"Player {current_row} of {rowcount} added to dataframe...")    
+        time.sleep(30)
+        browser = webdriver.Chrome()
+        browser.get(url)
+        time.sleep(5)
+        html = browser.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        race_info = soup.find('strong', string='Main Race').next_sibling.find_next(string=True)
 
-if current_row != rowcount:
-    print(f"Warning: Only {current_row} of {rowcount} players' races has been loaded to the dataframe.")
+        idx = row.Index
+        df_players.at[idx, 'player_race'] = race_info
+        current_row += 1
+        player_added += 1
+        print(f"Player {current_row} of {rowcount}, username {row.player_name} added to dataframe...")    
+    except:
+        print(f"Player {current_row} of {rowcount}, username {row.player_name}_{row.player_id} failed to be added...")
+        errors_on_add.append(row.player_id)
+        current_row += 1
+        pass
+        
+if player_added != rowcount:
+    print(f"Warning: Only {player_added} of {rowcount} players' races has been loaded to the dataframe.")
+    print(f"Warning: Players {errors_on_add} have not been added. These will require manual fixes.")
+    
 else:
-    print(f"Success: All {current_row} of {rowcount} players' races has been loaded to the dataframe.")
+    print(f"Success: All {player_added} of {rowcount} players' races has been loaded to the dataframe.")
 
 df_players.to_csv('../data/players.csv', index=False)
